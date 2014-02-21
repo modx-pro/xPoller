5fe6a8e9aad3c4600a3e837d993cd76330a03be5
Ilya Utkin <ilyautkin@mail.ru>
2014-02-21 15:14:19 +0400

    Deleted AjaxFrom plugin

--- core/components/xpoller/model/xpoller/mysql/xpolleritem.class.php
+++
@@ -1,3 +0,0 @@
-<?php
-require_once (dirname(dirname(__FILE__)) . '/xpolleritem.class.php');
-class xPollerItem_mysql extends xPollerItem {}
--- core/components/xpoller/model/xpoller/xpolleritem.class.php
+++
@@ -1,5 +0,0 @@
-<?php
-/**
- * @package xpoller
- */
-class xPollerItem extends xPDOSimpleObject {}
